6cf6b7476144b6c3fd49754a090b345ccc4c189f
Morgan Dilling <morgandilling171@gmail.com>
2023-11-16 11:32:24 +0000

    feat: Add v1 index

---
+++ src/v1/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    'is-alive': true,
+  });
+});
+
+export default router;
